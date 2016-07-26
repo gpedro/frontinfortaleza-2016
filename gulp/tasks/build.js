b53542b073699e5c5007eb27e609fb7b209bff18
vitormalencar <vitor.maia.alencar@gmail.com>
2016-07-26 13:55:23 -0300

    update suport

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -7,7 +7,7 @@ import gulpLoadPlugins from 'gulp-load-plugins';
 // Constants
 const $ = gulpLoadPlugins();
 gulp.task('build', () => {
-  runSequence(['html', 'images', 'extras', 'font-awesome','critical']);
+  runSequence(['html', 'images', 'extras', 'font-awesome', ], 'critical');
   return gulp.src(paths.build.root)
     .pipe($.size({
       title: 'build',
